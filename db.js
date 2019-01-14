103aa296de6059a4aeb6e5bd8ecf0a4aa4f4e950
godzyken <isgodzy@msn.com>
2019-01-14 18:08:41 +0100

    refonte de la moulinette
    Signed-off-by: emryck <emryck@appsvelocity.com>

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 var mysql = require('mysql');
 
 var connection = mysql.createPool({
-    host: 'localhost',
-    user: 'tutosme.dev',
-    password: 'HNmB1g1KWEODsI2u',
-    database: 'tutosme.dev'
+    host: 'http://52.47.104.12/mysqlCloudManager/',              // Lien de la bdd - cloud
+    user: 'tutosme.dev',                                        // Loggin
+    password: 'HNmB1g1KWEODsI2u',                               // MDP
+    database: 'tutosme.dev',                                    // Bdd dev
+    server: 'appsvelocity.cabutdpbsmsc.eu-west-3.rds.amazonaws.com' // serveur de dev
 });
 module.exports = connection;
--- gold/gold.js
+++ gold/gold.js
@@ -1,13 +1,10 @@
 var db = require('../db');
-var XLSX = require('xlsx');
-var wb = new XLSX.readFile('./gold/BBD Gold insert.xlsx', {sheetStubs: true});
-var rows = wb.SheetNames;
+var GoldSQL = require('./GoldToSql');
 
-console.log(XLSX.utils.sheet_to_json(wb.Sheets[rows[0]]));
 
-var Gold = {
+var golds = {
     getGold: function (callback) {
-        return db.query('SELECT * FROM user_center', callback);
+        return GoldSQL;
     },
     createGold: function (Gold, callback) {
         return db.query('INSERT INTO user_center(' +
@@ -33,48 +30,6 @@ var Gold = {
                 Gold.siret
             ], callback);
     }
-}
+};
 
-rows.forEach(function (y) {
-
-    var worksheet = wb.Sheets[y];
-    var headers = {};
-    var data = [];
-
-    for (cellule in worksheet) {
-        if (cellule[0] === '!') continue;
-        // parse la colonne, ligne, valeur
-
-        var tt = 0;
-        for (var i = 0; i < cellule.length; ++i) {
-            if (!isNaN(cellule[i])) {
-                tt = i;
-                break;
-            }
-        }
-        ;
-
-        var col = cellule.substring(0, tt);
-        var row = parseInt(cellule.substring(tt));
-        var value = worksheet[cellule].w;
-
-
-        // enregistre les tetes de colonne (titres: headers)
-        if (row == 1 && value) {
-            headers[col] = value;
-            continue;
-        }
-
-
-        if (!data[row]) data[row] = {};
-        data[row][headers[col]] = value;
-    }
-    //
-
-    data.shift();
-    data.shift();
-    console.log(data);
-});
-
-
-module.exports = rows;
+module.exports = golds;
