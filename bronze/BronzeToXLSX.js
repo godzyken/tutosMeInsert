e380e483c9ffc70723a62a88b087623e8e6ca27c
godzyken <isgodzy@msn.com>
2019-01-15 00:04:29 +0100

    refonte de la moulinette
    Signed-off-by: emryck <emryck@appsvelocity.com>
    Signed-off-by: godzyken <isgodzy@msn.com>

---
+++ bronze/BronzeToXLSX.js
@@ -0,0 +1,46 @@
+var XLSX = require('xlsx');
+var wb = new XLSX.readFile('./gold/goldz.xlsx', {sheetStubs: true});
+var bronzeToXLSX = wb.SheetNames;
+
+bronzeToXLSX.forEach(function (y) {
+
+    var worksheet = wb.Sheets[y];
+    var headers = {};
+    var data = [];
+
+    for (cellule in worksheet) {
+        if (cellule[0] === '!') continue;
+        // parse la colonne, ligne, valeur
+
+        var tt = 0;
+        for (var i = 0; i < cellule.length; ++i) {
+            if (!isNaN(cellule[i])) {
+                tt = i;
+                break;
+            }
+        }
+        ;
+
+        var col = cellule.substring(0, tt);
+        var row = parseInt(cellule.substring(tt));
+        var value = worksheet[cellule].w;
+
+
+        // enregistre les tetes de colonne (titres: headers)
+        if (row == 1 && value) {
+            headers[col] = value;
+            continue;
+        }
+
+
+        if (!data[row]) data[row] = {};
+        data[row][headers[col]] = value;
+    }
+    //
+
+    data.shift();
+    data.shift();
+    console.log(data);
+});
+
+module.exports = bronzeToXLSX;
--- db.js
+++ db.js
@@ -1,11 +1,6 @@
 var mysql = require('mysql');
 
 var connection = mysql.createPool({
-    host: 'http://52.47.104.12/mysqlCloudManager/',              // Lien de la bdd - cloud
-    user: 'tutosme.dev',                                        // Loggin
-    password: 'HNmB1g1KWEODsI2u',                               // MDP
-    database: 'tutosme.dev',                                    // Bdd dev
-    server: 'appsvelocity.cabutdpbsmsc.eu-west-3.rds.amazonaws.com' // serveur de dev
     host: 'localhost:3306',                     // Lien de la bdd - cloud http://52.47.104.12/mysqlCloudManager/
     user: 'root',                               // Loggin : tutosme.dev
     password: '',                               // MDP : HNmB1g1KWEODsI2u
