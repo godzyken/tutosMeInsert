f157e4f3f48735c9dec41ff11abe76db423bef7d
godzyken <isgodzy@msn.com>
2019-01-23 16:12:25 +0100

    moulinette evoluition par emryck:
    task:
     - test d'insertion
    
     TODO:
     - Creer des path en Variable global
     - insertion de l'url en base
    
    Signed-off-by: emryck <emryck@appsvelocity.com>

--- gold/index.js
+++ gold/index.js
@@ -1,6 +1,7 @@
 const XlsxExtractor = require('../utils/xlsxExtractor');
 const fs = require('fs');
 const path = require('path');
+const checksum = require('checksum');
 
 
 /* -------------------------------------------------- */
@@ -34,9 +35,9 @@ const columns = {
 module.exports = async (Models) => {
 
     // script d'extraction de données
-    const {headers, rows} = XlsxExtractor("./BBD gold/BBD gold/BBD Gold.xlsx");
+    const {headers, rows} = XlsxExtractor("./BBD gold/BBD gold/BBD Gold.xlsx");        // TODO make it Global Variable
 
-    // Parse les données du fichier xlsx
+    // Parse les données nom, prenom du fichier xlsx
     function saniTize(origin) {
         let pattern = " ",
             re = new RegExp(pattern, "g");
@@ -44,43 +45,55 @@ module.exports = async (Models) => {
         return origin.split(re).join("");
     }
 
+    // Creer le lien URL de la photo-profile
+    function createUrl(index, data) {
+        let urlbase = ('https://s3.eu-west-3.amazonaws.com/tutosmebackoffice/trainer/'); // TODO make it Global Variable
+        let pathPics = index + "_" + checksum(data);
+        let ext = '.jpeg';
+        let Uri = path.join(urlbase, pathPics + ext);
+
+        return Uri;
+    }
 
     // injitialise les repertoires de recherche
-    let pathFilePicture = 'BBD gold/BBD gold/Photos/';
-    let pathFileCV = 'BBD gold/BBD gold/CV';
+    let pathFilePicture = 'BBD gold/BBD gold/Photos/';  // TODO make it Global Variable
+    let pathFileCV = 'BBD gold/BBD gold/CV';            // TODO make it Global Variable
 
-    for (row of rows) {
+    //for (row of rows) {
+    for (let index = 0; index < rows.length; index++) {
 
         const trainer = Models.Trainer.build();
-        trainer.email = row[headers[columns.email]];
-        trainer.first_name = row[headers[columns.first_name]];
-        trainer.last_name = row[headers[columns.last_name]];
-        trainer.address = row[headers[columns.address]];
-        trainer.ccp = row[headers[columns.ccp]];
-        trainer.ville = row[headers[columns.ville]];
-        trainer.mobile_phone = row[headers[columns.mobile_phone]];
-        trainer.picture = row[headers[columns.picture]] || "";
-        trainer.nomCv = row[headers[columns.nomCV]];
-        trainer.matieres = row[headers[columns.matieres]];
-        trainer.siret = row[headers[columns.siret]];
-
-        // trainer.user_id = Models.User.id;
+        trainer.email = rows[index][headers[columns.email]];
+        trainer.first_name = rows[index][headers[columns.first_name]];
+        trainer.last_name = rows[index][headers[columns.last_name]];
+        trainer.address = rows[index][headers[columns.address]];
+        trainer.ccp = rows[index][headers[columns.ccp]];
+        trainer.ville = rows[index][headers[columns.ville]];
+        trainer.mobile_phone = rows[index][headers[columns.mobile_phone]];
+        trainer.picture = rows[index][headers[columns.picture]] || "";
+        trainer.nomCv = rows[index][headers[columns.nomCV]];
+        trainer.matieres = rows[index][headers[columns.matieres]];
+        trainer.siret = rows[index][headers[columns.siret]];
 
 
         // Recherche par Nom de fichier
         if (trainer.picture && trainer.picture !== "") {
 
-            // Construit le Nom de l'image du formateur
+            // Construit le Nom de l'image de l'utilisateur
             let names = trainer.first_name + trainer.last_name;
 
+            // Formate le nom de l'image
             let filename = saniTize(names);
 
+            // Construit l'url de la photo a sauvegarder
+            let url = createUrl(index, filename);
 
             // Parcours le répertoire source d'images
             let src = path.join(pathFilePicture, trainer.picture);
 
             // Construit le nom du repertoire destinataire
-            let destDir = path.join(__dirname, '/formateur/' + saniTize(filename));
+            let destDir = path.join(__dirname, '/formateur/' + filename);
+
             fs.access(destDir, (err) => {
                 if (err) {
                     console.log(err);
@@ -88,6 +101,7 @@ module.exports = async (Models) => {
                 }
                 copyFile(src, path.join(destDir, filename + '.png'));
             });
+
 
             // Copie l'image dans le répertoire destinataire
             function copyFile(src, dest) {
@@ -99,13 +113,18 @@ module.exports = async (Models) => {
                 });
 
                 readStream.once('end', () => {
+
+                    console.log('------go---------go---------go---------------');
                     console.log('copy ok pour le formateur:' + filename);
                     console.log("Src : ", src);
-                    console.log("Dest : ", dest)
+                    console.log("Dest : ", dest);
+                    console.log("Url : ", url);
+                    console.log('------ok---------ok---------ok---------------');
                 });
 
                 readStream.pipe(fs.createWriteStream(dest));
             }
+
 
         }
         else {
@@ -156,6 +175,7 @@ module.exports = async (Models) => {
             console.log("pas de cv disponible !")
         }
 
+        trainer.picture = createUrl(url);
         // sauvegarder chaque utilisateur dans la bdd
         await trainer.save();
 
